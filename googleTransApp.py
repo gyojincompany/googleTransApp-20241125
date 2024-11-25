0b83d351ca6958d235977a908cb5b61233bdfb0d
gyojincompany <luckyred@gmail.com>
2024-11-25 11:38:32 +0900

    공백 입력 방지 경고창 기능 추가

--- googleTransApp.py
+++ googleTransApp.py
@@ -26,14 +26,17 @@ class GoogleTransApp(QMainWindow, form_class):
         # print("번역실행 버튼이 클릭되었습니다!!")
         kor_input_text = self.kor_input_edit.text()  # 해당 line edit 에 입력된 텍스트 가져오기
 
-        trans = googletrans.Translator()  # 구글 번역 객체 생성
-        resultEng = trans.translate(kor_input_text, dest="en")  # 영어 번역 결과
-        resultJap = trans.translate(kor_input_text, dest="ja")  # 일본어 번역 결과
-        resultChn = trans.translate(kor_input_text, dest="zh-cn")  # 중국어 번역 결과
+        if kor_input_text == "":  # 입력된 내용이 공백인지 확인
+            QMessageBox.warning(self, "입력오류!","한글입력란에 반드시\n문장을 넣어주세요.")
+        else:
+            trans = googletrans.Translator()  # 구글 번역 객체 생성
+            resultEng = trans.translate(kor_input_text, dest="en")  # 영어 번역 결과
+            resultJap = trans.translate(kor_input_text, dest="ja")  # 일본어 번역 결과
+            resultChn = trans.translate(kor_input_text, dest="zh-cn")  # 중국어 번역 결과
 
-        self.eng_output_edit.append(resultEng.text)  # 영어 출력
-        self.jap_output_edit.append(resultJap.text)  # 일본어 출력
-        self.chn_output_edit.append(resultChn.text)  # 중국어 출력
+            self.eng_output_edit.append(resultEng.text)  # 영어 출력
+            self.jap_output_edit.append(resultJap.text)  # 일본어 출력
+            self.chn_output_edit.append(resultChn.text)  # 중국어 출력
 
     def remove_input_edit(self):  # 입력 에디트 초기화 메소드
         self.kor_input_edit.clear()  # 해당 라인 에디트의 내용 삭제
